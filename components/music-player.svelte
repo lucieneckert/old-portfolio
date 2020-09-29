ff775d338f072efa4451e5b273074c128bde106c
lucieneckert <lucien.eckert@gmail.com>
2020-09-29 17:14:30 -0400

    Dumping current portfolio progress into the repo

---
+++ components/music-player.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { each } from "svelte/internal";
+
+  let musicfolder;
+  let tracks = [0, 1, 2];
+  let currenttrack = "no track selected";
+</script>
+
+<ul>
+  {#each tracks as track}
+    <li>
+      <button onclick="document.getElementById('{currenttrack}').play()">
+        ▶
+      </button>
+      <button onclick="document.getElementById('{currenttrack}').pause()">
+        ⏸
+      </button>
+      {currenttrack}<audio id={currenttrack} src={track} preload="none" />
+    </li>
+  {/each}
+</ul>
